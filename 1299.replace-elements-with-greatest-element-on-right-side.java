8511dcd01c69c3a67f92bed92b741d7dd61b0993
cutety <846601604@qq.com>
2020-07-01 15:11:57 +0800

    Create 1299.replace-elements-with-greatest-element-on-right-side.java
    LeetCode 1299  Replace Elements with Greatest Element on Right Side

---
+++ 1299.replace-elements-with-greatest-element-on-right-side.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=1299 lang=java
+ *
+ * [1299] Replace Elements with Greatest Element on Right Side
+ */
+
+// @lc code=start
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int max=arr[arr.length-1];
+        arr[arr.length-1]=-1;
+        for(int i=arr.length-2;i>=0;i--){
+           int nextMax=Math.max(max, arr[i]);
+           arr[i]=max;
+           max=nextMax;
+        }
+        return arr;
+    }
+}
+// @lc code=end
+
